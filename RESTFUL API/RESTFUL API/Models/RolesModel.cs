71796c7fa308b5b6a92497f48a9acddda401f0ef
RODOLFO-SOLANO\Esteban <rsolano1996@gmail.com>
2017-10-07 06:10:52 -0600

    rest

---
+++ RESTFUL API/RESTFUL API/Models/RolesModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RESTFUL_API.Models
+{
+    public class RolesModel
+    {
+        public Nullable<int> idRol { get; set; }
+        public String Nombre { get; set; }
+        public String Descripcion { get; set; }
+    }
+}
