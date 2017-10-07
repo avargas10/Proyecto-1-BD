b83b95fe5505549dde4881a279815341e0ef7f24
avargas10 <afelipe.vargas.r@gmail.com>
2017-10-07 11:25:04 -0600

    web page

--- WebPage/Front-End/Scripts/script.js
+++ WebPage/Front-End/Scripts/script.js
@@ -1,14 +1,23 @@
-var app = angular.module("mainModule",["ngRoute",'vcRecaptcha']);
+var app = angular.module("mainModule",
+["ngRoute",
+'vcRecaptcha',
+'uiGmapgoogle-maps']);
 
-app.config(function($routeProvider, $locationProvider){
+app.config(function(uiGmapGoogleMapApiProvider,$routeProvider, $locationProvider){
 	$routeProvider.
 	when('/Home', {templateUrl: '../Views/home-view.html', controller: 'mainController'}).
 	when('/login',{templateUrl: '../Views/login-view.html',controller: 'userController'}).
 	when('/register',{templateUrl: '../Views/register.html',controller: 'userController'}).
   when('/pedido',{templateUrl: '../Views/pedido.html',controller: 'userController'}).
   when('/products',{templateUrl: '../Views/client.html',controller: 'userController'}).
+  when('/stores',{templateUrl: '../Views/stores.html',controller: 'mainController'}).
 		otherwise({ redirectTo: '/Home'});
-	// $locationProvider.html5Mode(true);
+  // $locationProvider.html5Mode(true);
+  uiGmapGoogleMapApiProvider.configure({
+    key: 'AIzaSyAIPxUS3YTey5p2garlnWopeev1MW5gtfY',
+    v: 3, 
+    libraries: 'weather,geometry,visualization'
+});
 });
 
 app.service('userService', function() {
@@ -32,6 +41,39 @@ app.service('userService', function() {
 	}
 });
 
+app.service('directionService', function() {
+  var state;
+  var district;
+  var city;
+  var id;
+
+    this.setId = function(pId) {
+      console.log("id: "+pId);
+      id= pId;		
+  }
+    this.getId = function(){return id;}
+    this.setState = function(pState) {
+		  state= pState;		
+		}
+    this.setCity = function(pCity) {
+		  city= pCity;		
+    }
+    this.setDisctrict = function(pDistrict) {
+		  district= pDistrict;		
+    }
+    this.getState=function(){return state;}
+    this.getCity=function(){return city;}
+    this.getDistrict=function(){return district;}
+    
+});
+app.service('storeService', function() {
+  var store;
+    this.setStore = function(pStore) {
+		  store=pStore;		
+		}
+    this.getStore=function(){return store;}
+    
+});
 
 app.controller("mainController",["$scope","$http","$location","$routeParams",'userService',
 
@@ -65,9 +107,14 @@ function($scope,$http,$location,$routeParams,userService){
             case 'signup':
                 $location.path("/register");
                 break;
+            case 'stores':
+                $location.path("/stores");
+                break;
             default:
         }
   };
+
+
   $scope.getHttp= function(url , callback){
     var httpObject = $http.get(url);
     httpObject.then(function(promise){
@@ -80,10 +127,10 @@ function($scope,$http,$location,$routeParams,userService){
     callback(promise.data);
     }, function(error){ console.log(error);})}
   }
- 
 ]);
-app.controller("userController",["$scope","$http","$location","$routeParams","userService",
-function($scope,$http,$location,$routeParams,userService){
+
+app.controller("userController",["$scope","$http","$location","$routeParams","userService",'directionService',
+function($scope,$http,$location,$routeParams,userService,directionService){
   var states;
   var cities;
   var districts;
@@ -102,7 +149,7 @@ function($scope,$http,$location,$routeParams,userService){
   function isBlank(str) {
     return (!str || /^\s*$/.test(str));
   }
-
+ 
 
   $scope.loginUser = function(username,password,getCaptcha){
   	if(!isBlank(username)  && !isBlank(password) ){
@@ -120,53 +167,139 @@ function($scope,$http,$location,$routeParams,userService){
     	alert("Error(01): Can't sign in, space in blank or not getCaptcha");
     }
 };
-
 $scope.UpdateDirection = function(){
+  console.log("direction update");
     var url = 'http://localhost:58706/api/Provincias';
-    $http.get(url).then(function(msg){
-    		states = msg.data;
-    	});
-    $scope.states = states;
-
+    $http.get(url)
+    .then(function successCallback(data) {
+      console.log(data);
+         $scope.states = data.data;
+     },
+     function errorCallback(response) {
+         alert(response);
+     });
+  };
+ $scope.UpdateCities = function(_id){
+  directionService.setState(_id);
+    var url = 'http://localhost:58706/api/Cantones?idProvincia='+_id;
+    $http.get(url)
+    .then(function successCallback(data) {
+      console.log(data);
+         $scope.cities = data.data;
+     },
+     function errorCallback(response) {
+         alert(response);
+     });
 };
-$scope.UpdateCities = function(_id){
-    var url = 'http://localhost:58706/api/Cantones?Provincia='+_id;
-    $http.get(url).then(function(msg){
-        cities = msg.data;
-        console.log(cities);
-    	});
-    $scope.cities = cities;
+$scope.UpdateDistricts = function(_id){
+  directionService.setCity(_id);
+    var url = 'http://localhost:58706/api/Distrito?idCanton='+_id;
+    $http.get(url)
+    .then(function successCallback(data) {
+      console.log(data);
+         $scope.districts = data.data;
+     },
+     function errorCallback(response) {
+         alert(response);
+     });
 };
-
-
-}]);
-
-app.controller("contPedido", 
-  function($scope) {
-    $scope.id = "";
-    $scope.tel="";
-    $scope.sucursal="";
-    $scope.fecha="";
-    $scope.listProducts=[
-      {'img':'../images/med.png', 'producto': 'Tabcin' ,'precio': '40', 'prescripcion': 'No' },
-      {'img':'../images/iny.png', 'producto': 'Gex' ,'precio': '10', 'prescripcion': 'No' },
-      {'img':'../images/med.png', 'producto': 'Talerdin' ,'precio': '30', 'prescripcion': 'No' },
-      {'img':'../images/iny.png', 'producto': 'Inyeccion' ,'precio': '100', 'prescripcion': 'Si' },
-      {'img':'../images/med.png', 'producto': 'Suero' ,'precio': '60', 'prescripcion': 'Si' }
-    ];
-
-
-    $scope.submitCart=function(){
-      alert($scope.sucursal+','+$scope.fecha);
-      animation();
-    }
-
+$scope.setDistrict = function(_id){
+      directionService.setDisctrict(_id);
+};
+$scope.createUser= function(username,password,conPassword,name,surname,sSurname,id,dir,date,email){
+  
+  if(createValidation(username,password,conPassword,name,surname,sSurname,id,date,email))
+  {if(password==conPassword){
+    var url ='http://localhost:58706/api/Clientes';
+    var sendData = {
+      "Cedula": parseInt(id),
+      "Nombre": name,
+      "pApellido": surname,
+      "sApellido": sSurname,
+      "Password": password,
+      "Username": username,
+      "Email": email,
+      "Nacimiento": date,
+      "Penalizacion": 0,
+      "Direccion": dir,
+      "Estado": 1
+    };
+    $http.post(url,sendData)
+    .success(function(data) {
+      console.log(data);
+      console.log(sendData);
+      if(data.Cedula==sendData.Cedula)
+      {
+        $location.path("/login");
+      }
+    })
+    .error(function(data, status) {
+      console.error('Repos error', status, data);
+    })
+    .finally(function() {
+      console.log("finally finished repos");
+    });
   }
+    else{
+      alert("Error(03): Passwords not match");
+    }
+  }
+  else{ alert("Error(01): Can't sign in, space in blank");}
+  
+ 
+};
 
-);
+$scope.setDirection =function(username,password,conPassword,name,surname,sSurname,id,dirSpec,date,email){
+  var url ='http://localhost:58706/api/Direcciones';
+  var sendData = {
+    "Provincia": directionService.getState(),
+    "Canton": directionService.getCity(),
+    "Distrito": directionService.getDistrict(),
+    "Descripcion": dirSpec
+  };
+  $http.post(url,sendData)
+  .success(function(data) {
+    $scope.createUser(username,password,conPassword,name,surname,sSurname,id,data.idDireccion,date,email);
+  })
+  .error(function(data, status) {
+    console.error('Repos error', status, data);
+  })
+  .finally(function() {
+    console.log("finally finished evaluating");
+  });
+};
 
-function animation() {
-    var x = document.getElementById("snackbar")
-    x.className = "show";
-    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+function createValidation(username,password,conPassword,name,surname,sSurname,id,date,email){
+  if(!isBlank(username) && !isBlank(password) && !isBlank(conPassword) && !isBlank(name) && !isBlank(surname)&&
+  !isBlank(sSurname) && !isBlank(id)  && !isBlank(date) &&!isBlank(email))
+    {return true;}
+  else
+  {
+    return false;}
+};
 }
+]);
+
+app.controller("storeController",["uiGmapGoogleMapApi","$scope","$http","$location","$routeParams","userService",'directionService',
+function(uiGmapGoogleMapApi,$scope,$http,$location,$routeParams,userService,directionService){
+  $scope.map = {center: {latitude: 10.9517300, longitude: -85.1361000 }, zoom: 14 };
+  $scope.options = {scrollwheel: false};
+  $scope.showStore = function(latitud,longitude){
+  console.log(latitud);
+  $scope.map = {center: {latitude: latitud, longitude: longitude }, zoom: 14 };
+  $scope.options = {scrollwheel: false};
+}
+
+
+$scope.getStores = function(){
+  var url = 'http://localhost:58706/api/Sucursal';
+  $http.get(url)
+  .then(function successCallback(data) {
+    console.log(data);
+       $scope.stores = data.data;
+   },
+   function errorCallback(response) {
+       alert(response);
+   });
+};   
+}]);
