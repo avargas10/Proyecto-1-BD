028b96a72fba0b3359fedb6b3c0df08ec7bdeb8f
adrian039 <adrian_sa12@outlook.com>
2017-10-08 23:43:16 -0600

    cambio

--- AndroidApp/gspApp/app/src/main/java/com/example/adrian/gspapp/nuevaReceta.java
+++ AndroidApp/gspApp/app/src/main/java/com/example/adrian/gspapp/nuevaReceta.java
@@ -1,8 +1,6 @@
 package com.example.adrian.gspapp;
 
 import android.app.Activity;
-import android.app.Fragment;
-import android.app.FragmentTransaction;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
