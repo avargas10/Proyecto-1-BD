16a6e65f8a9da151f3dca114043536fb39ea5f8f
RODOLFO-SOLANO\Esteban <rsolano1996@gmail.com>
2017-10-09 22:41:50 -0600

    app + rest

--- AndroidApp/gspApp/app/src/main/java/com/example/adrian/gspapp/Registro.java
+++ AndroidApp/gspApp/app/src/main/java/com/example/adrian/gspapp/Registro.java
@@ -99,7 +99,7 @@ public class Registro extends AppCompatActivity {
                     data.put("Direccion", Connection.getInstance().registroDireccion(direccion).get("idDireccion"));
                     if(Connection.getInstance().registroCliente(data)){
                         Toast.makeText(getApplicationContext(),"Usuario registrado con exito", Toast.LENGTH_SHORT).show();
-                        Connection.getInstance().ActivationEmail(username.getText().toString());
+                        Connection.getInstance().ActivationEmail(Integer.parseInt(cedula.getText().toString()));
                         username.setText("");
                         pass.setText("");
                         name.setText("");
