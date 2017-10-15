ccea044d6d9a02098d0f7bac9d6b574042eb4b9d
adrian039 <adrian_sa12@outlook.com>
2017-10-14 22:51:22 -0600

    as

--- RESTFUL API/RESTFUL API/Controllers/SucursalController.cs
+++ RESTFUL API/RESTFUL API/Controllers/SucursalController.cs
@@ -19,7 +19,7 @@ namespace RESTFUL_API.Controllers
         {
             using (SqlConnection conn = new SqlConnection(DatabaseConnectionString))
             {
-                SqlCommand cmd = new SqlCommand("SELECT idSucursal,idEmpresa,idProvincia,idCanton,idDistrito,Latitud,Longitud, detalleDireccion,Nombre, Imagen FROM SUCURSAL", conn);
+                SqlCommand cmd = new SqlCommand("SELECT idSucursal,idEmpresa,idProvincia,idCanton,idDistrito, detalleDireccion,Nombre, Imagen FROM SUCURSAL", conn);
                 cmd.Connection = conn;
                 conn.Open();
                 using (var reader = cmd.ExecuteReader())
@@ -37,7 +37,7 @@ namespace RESTFUL_API.Controllers
         {
             using (SqlConnection conn = new SqlConnection(DatabaseConnectionString))
             {
-                SqlCommand cmd = new SqlCommand("SELECT idSucursal,idEmpresa,idProvincia,idCanton,idDistrito,Latitud,Longitud, detalleDireccion,Nombre, Imagen FROM SUCURSAL WHERE idEmpresa=@id", conn);
+                SqlCommand cmd = new SqlCommand("SELECT idSucursal,idEmpresa,idProvincia,idCanton,idDistrito, detalleDireccion,Nombre, Imagen FROM SUCURSAL WHERE idEmpresa=@id", conn);
                 cmd.Parameters.AddWithValue("@id", empresa);
                 cmd.Connection = conn;
                 conn.Open();
@@ -55,7 +55,7 @@ namespace RESTFUL_API.Controllers
         {
             using (SqlConnection conn = new SqlConnection(DatabaseConnectionString))
             {
-                SqlCommand cmd = new SqlCommand("SELECT idSucursal,idEmpresa,idProvincia,idCanton,idDistrito,Latitud,Longitud, detalleDireccion,Nombre FROM SUCURSAL WHERE idSucursal=@id", conn);
+                SqlCommand cmd = new SqlCommand("SELECT idSucursal,idEmpresa,idProvincia,idCanton,idDistrito, detalleDireccion,Nombre FROM SUCURSAL WHERE idSucursal=@id", conn);
                 cmd.Parameters.AddWithValue("@id", id);
                 cmd.Connection = conn;
                 conn.Open();
@@ -88,13 +88,11 @@ namespace RESTFUL_API.Controllers
                     else
                     {
                         conn.Close();
-                        SqlCommand cmd = new SqlCommand("INSERT INTO SUCURSAL (idEmpresa,idProvincia,idCanton,idDistrito,Latitud,Longitud, detalleDireccion,Nombre,Estado) VALUES (@empresa,@provincia,@canton,@distrito,@lat,@long,@detalle,@nombre,@estado)", conn);
+                        SqlCommand cmd = new SqlCommand("INSERT INTO SUCURSAL (idEmpresa,idProvincia,idCanton,idDistrito, detalleDireccion,Nombre,Estado) VALUES (@empresa,@provincia,@canton,@distrito,@detalle,@nombre,@estado)", conn);
                         cmd.Parameters.AddWithValue("@empresa", suc.idEmpresa);
                         cmd.Parameters.AddWithValue("@provincia", suc.idProvincia);
                         cmd.Parameters.AddWithValue("@canton", suc.idCanton);
                         cmd.Parameters.AddWithValue("@distrito", suc.idDistrito);
-                        cmd.Parameters.AddWithValue("@lat", suc.Latitud);
-                        cmd.Parameters.AddWithValue("@long", suc.Longitud);
                         cmd.Parameters.AddWithValue("@detalle", suc.detalleDireccion);
                         cmd.Parameters.AddWithValue("@nombre", suc.Nombre);
                         cmd.Parameters.AddWithValue("@estado", suc.Estado);
