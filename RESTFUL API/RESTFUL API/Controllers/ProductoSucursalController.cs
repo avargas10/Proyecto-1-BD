69ca171014ad10e6ad0eec794c162cfa9ea3e9ca
RODOLFO-SOLANO\Esteban <rsolano1996@gmail.com>
2017-10-17 16:38:36 -0600

    rest

---
+++ RESTFUL API/RESTFUL API/Controllers/ProductoSucursalController.cs
@@ -0,0 +1,45 @@
+﻿using RESTFUL_API.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace RESTFUL_API.Controllers
+{
+    public class ProductoSucursalController : ApiController
+    {
+        JSONSerializer serial = new JSONSerializer();
+        string DatabaseConnectionString = System.Configuration.ConfigurationManager.ConnectionStrings["GasStationPharmacyDB"].ConnectionString;
+
+
+        [HttpPost]
+        public HttpResponseMessage regProductoSucursal(productoSucursalModel ProdSucursal)
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(DatabaseConnectionString))
+                {
+                    SqlCommand cmd = new SqlCommand("INSERT INTO PRODUCTOXSUCURSAL(idSucursal, codProducto, Cantidad, Precio) VALUES (@id,@producto,@cantidad,@precio)", conn);
+                    cmd.Parameters.AddWithValue("@id", ProdSucursal.idSucursal);
+                    cmd.Parameters.AddWithValue("@producto", ProdSucursal.codProducto);
+                    cmd.Parameters.AddWithValue("@cantidad", ProdSucursal.Cantidad);
+                    cmd.Parameters.AddWithValue("@precio", ProdSucursal.Precio);
+                    cmd.Connection = conn;
+                    conn.Open();
+                    cmd.ExecuteReader();
+                    var message = Request.CreateResponse(HttpStatusCode.Created, ProdSucursal);
+                    return message;
+                }
+
+            }
+            catch (Exception e)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, e);
+            }
+
+        }
+    }
+}
